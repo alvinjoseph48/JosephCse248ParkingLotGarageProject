2f074561452b8b81ecacc82e614710269c771cd0
alvinjoseph48 <alvinjoseph48@users.noreply.github.com>
2017-10-02 19:36:00 -0400

    Setting up Panes

--- src/view/ParkingLotView.java
+++ src/view/ParkingLotView.java
@@ -1,8 +1,13 @@
 package view;
 
+import java.util.Stack;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Car;
+import model.Motercycle;
+import model.Truck;
 
 public class ParkingLotView extends Application {
 
@@ -12,16 +17,31 @@ public class ParkingLotView extends Application {
 		ExitParkingLotPane exitParkingLotPane = new ExitParkingLotPane();
 		TicketPane ticketPane = new TicketPane();
 
+		Stack<Car> carStack = new Stack<Car>();
+		Stack<Motercycle> moterCycleStack = new <Motercycle>Stack();
+		Stack<Truck> truckStack = new Stack<Truck>();
+
+		// truckStack.setSize();
+		// carStack.setSize(10);
+		// moterCycleStack.setSize(20);
+
 		parkingPane.getMenuBar().prefWidthProperty().bind(primaryStage.widthProperty());
 
 		parkingPane.getParkingMenu().setOnAction(e -> {
+			parkingPane.getBorderPane().setCenter(exitParkingLotPane.getExitPane());
 
 		});
 		parkingPane.getBuyTicket().setOnAction(e -> {
-
+			parkingPane.getBorderPane().setCenter(ticketPane.getTicketPane());
 		});
 		parkingPane.getExit().setOnAction(e -> {
 			System.exit(0);
+		});
+		ticketPane.getBuyTicketBtn().setOnAction(e -> {
+
+		});
+		exitParkingLotPane.getConfirmBtn().setOnAction(e -> {
+
 		});
 
 		Scene myScene = new Scene(parkingPane.getBorderPane(), 500, 500);
